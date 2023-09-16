8838f59a0addbaf75c2f9fe1517c069c110165b0
kirti7464 <71482336+kirti7464@users.noreply.github.com>
2023-09-16 10:58:57 +0530

    Update Header.js

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,10 +3,10 @@ import React from 'react'
 const Header = () => {
   return (
     <div>
-      <div class="d-flex bd-highlight ">
-        <div class="me-auto p-1 bd-highlight">UTube Transcript</div>
+      <div className="d-flex bd-highlight ">
+        <div className="me-auto p-1 bd-highlight">UTube Transcript</div>
         
-        <div class="p-1 bd-highlight">Try this free tool to get transcripts.</div>
+        <div className="p-1 bd-highlight">Try this free tool to get transcripts.</div>
       </div>
     </div>
   )

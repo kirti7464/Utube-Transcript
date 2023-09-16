bced5a772eaf91b04d36e53999daf9abb3b013d8
kirti7464 <71482336+kirti7464@users.noreply.github.com>
2023-09-16 10:30:22 +0530

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Header from './components/Header';
+import { Sec } from './components/Sec';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <div className="sec">
+        <Sec/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
